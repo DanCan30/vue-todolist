f5c762d5dbcbac1381a60a687d02d1856e89ffd6
DanCan30 <danielecan3008@gmail.com>
2022-07-06 13:49:59 +0200

    Add task method added

--- script/main.js
+++ script/main.js
@@ -5,19 +5,23 @@ const app = new Vue (
 
     data: {
         todos: [
-        {
-            text: 'Fare i compiti',
-            done: false
-        },
-        {
-            text: 'Fare la spesa',
-            done: true
-        },
-        {
-            text: 'Fare il bucato',
-            done: false
-        }
-    ]
+            {
+                text: 'Fare i compiti',
+                done: false
+            },
+            {
+                text: 'Fare la spesa',
+                done: true
+            },
+            {
+                text: 'Fare il bucato',
+                done: false
+            }
+        ],
+
+        newTask: "",
+
+        placeholder: "Type a task...",
 
     },
 
@@ -25,6 +29,24 @@ const app = new Vue (
         
         removeFromList: function(list, index) {
             list.splice(index, 1);
+        },
+
+        addToList: function(list, itemToAdd) {
+
+            itemToAdd = {
+                text: this.newTask,
+                done: false,
+            }
+
+            if(this.newTask != "") {
+                list.push(itemToAdd);
+                this.newTask = "";
+                this.placeholder = "Type a task...";
+            } else {
+                this.placeholder = "Not valid!";
+            }
+            
+
         }
 
     }
