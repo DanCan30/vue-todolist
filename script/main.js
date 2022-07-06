288ba9901e5d0a5d06b9c1b49f1853a4ffdab6fa
DanCan30 <danielecan3008@gmail.com>
2022-07-06 14:10:06 +0200

    Removed placeholder tasks

--- script/main.js
+++ script/main.js
@@ -4,20 +4,7 @@ const app = new Vue (
     el: "#app",
 
     data: {
-        todos: [
-            {
-                text: 'Fare i compiti',
-                done: false
-            },
-            {
-                text: 'Fare la spesa',
-                done: true
-            },
-            {
-                text: 'Fare il bucato',
-                done: false
-            },
-        ],
+        todos: [],
 
         newTask: "",
 
